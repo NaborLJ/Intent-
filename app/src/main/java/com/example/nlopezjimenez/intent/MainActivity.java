14053007355c947dc305f322e7222d54ceae06e2
nlopezjimenez <naborlopezjimenez@gmail.com>
2016-11-25 12:20:49 +0100

    Eliminado metodo "SendMessage"

--- app/src/main/java/com/example/nlopezjimenez/intent/MainActivity.java
+++ app/src/main/java/com/example/nlopezjimenez/intent/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button) findViewById(R.id.button1);
 
 
-    }
-    public void SendMessage(View j){
-        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
-        startActivity(intent);
 
     }
+
 }
 
 
