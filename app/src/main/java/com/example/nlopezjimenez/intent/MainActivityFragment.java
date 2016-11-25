c108312233d57c863d65a0f2c0d8805474000642
nlopezjimenez <naborlopezjimenez@gmail.com>
2016-11-25 12:19:38 +0100

    Metodo cambiado a MainActivityFragment

--- app/src/main/java/com/example/nlopezjimenez/intent/MainActivityFragment.java
+++ app/src/main/java/com/example/nlopezjimenez/intent/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.nlopezjimenez.intent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,16 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+        View vista = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button boton = (Button) vista.findViewById(R.id.button1);
+
+    boton.setOnClickListener( new View.OnClickListener() {
+        public void onClick(View vista) {
+            Intent intent = new Intent(getActivity(), Main2Activity.class);
+            startActivity(intent);
+        }
+    });
+        return vista;
+}
 }
